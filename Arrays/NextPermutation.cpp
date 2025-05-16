3b345954d207223b8d530bac8936d09cc94ef4a9
Vaishnavi Agarwal <vaishnaviagarwal0910@gmail.com>
2025-05-16 16:05:04 +0530

    added Next Permutation

---
+++ Arrays/NextPermutation.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+    int ind=-1;
+    int n=nums.size();
+      for(int i=n-1;i>0;i--){
+        if(nums[i]>nums[i-1]){
+           ind =i-1; 
+           break;
+        }
+      }
+
+      if(ind==-1){
+        reverse(nums.begin(),nums.end());
+        return;
+      } 
+      int max=nums[ind+1];
+      for(int i=n-1;i>ind-1;i--){
+        if(nums[i]>nums[ind]){
+            swap(nums[i],nums[ind]);
+            break;
+        }
+      }
+        reverse(nums.begin()+ind+1,nums.end());
+
+    }
+};
+
+int main(){
+  vector<int> nums;
+  int x;
+
+  while(cin>>x && x!=-1){
+    nums.push_back(x);
+  }
+Solution s;
+s.nextPermutation(nums);
+for(int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+
+return 0;
+}
